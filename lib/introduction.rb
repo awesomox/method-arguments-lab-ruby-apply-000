f25b3c9b6f8474bb82cc1c571f09885b0e35037b
tom hannaford <thom.hannaford@gmail.com>
2019-11-30 20:53:49 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,6 @@
 
 # introduction_with_language
 
-def introduction("dan", "Ember.js")
+def introduction("Dan", "Ember.js")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
